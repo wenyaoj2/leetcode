5c27b0acdbc9da323377efd6285b4817221d9f8f
jinwenyao <wenyaojinapply@gmail.com>
2020-05-05 20:00:31 -0500

    387. First Unique Character in a String

---
+++ 387. First Unique Character in a String.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+class FirstUniqChar {
+    public int firstUniqChar(String s) {
+        HashMap<Character, Integer> hash = new HashMap();
+        for(char c : s.toCharArray()){
+            hash.put(c, hash.getOrDefault(c, 0) + 1);
+        }
+        for(int i = 0; i < s.length(); i++){
+            if(hash.get(s.charAt(i)) == 1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
