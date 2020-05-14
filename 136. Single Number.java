e3b63459e40b8732232bf4fd74fbc2b535c09ede
jinwenyao <wenyaojinapply@gmail.com>
2020-05-14 18:57:14 -0500

    Create 136.Single Number

--- 136. Single Number.java
+++ 136. Single Number.java
@@ -1,4 +1,6 @@
-class Solution {
+import java.util.*;
+
+class SingleNumber {
     public int singleNumber(int[] nums) {
         int last = 0;
         Map<Integer,Integer> ans = new HashMap();
@@ -15,10 +17,10 @@ class Solution {
 }
 
 //方法二使用^异或来解决奇偶问题
-0^0 = 0
-1^1 = 0
-0^a = a
-a^b^a = a^a^b = 0^b = b
+// 0^0 = 0
+// 1^1 = 0
+// 0^a = a
+// a^b^a = a^a^b = 0^b = b
 class Solution {
     public int singleNumber(int[] nums) {
         int ans = 0;
