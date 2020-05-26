c1f8953e02e413602a9d8d47546b1cd1af79761c
jinwenyao <wenyaojinapply@gmail.com>
2020-05-26 15:22:48 -0500

    Create 229. Majority Element II

---
+++ 229. Majority Element II.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        int num1 = 0;
+        int num2 = 0;
+        int count1 = 0;
+        int count2 = 0;
+        for(int num : nums) {
+            if(num == num1) {
+                count1++;
+            }
+            else if(num == num2) {
+                count2++;
+            }
+            else {
+                if(count1 == 0) {
+                    num1 = num;
+                    count1 = 1;
+                }
+                else if(count2 == 0) {
+                    num2 = num;
+                    count2 = 1;
+                }
+                else {
+                    count1--;
+                    count2--;
+                }
+            }
+        }
+        count1 = 0;
+        count2 = 0;
+        for(int num : nums) {
+            if(num == num1) {
+                count1++;
+            }
+            else if(num == num2) {
+                count2++;
+            }
+        }
+        List<Integer> list = new ArrayList();
+        if(count1 > nums.length / 3) {
+            list.add(num1);
+        }
+        if(count2 > nums.length / 3) {
+            list.add(num2);
+        }
+        return list;
+    }
+}
