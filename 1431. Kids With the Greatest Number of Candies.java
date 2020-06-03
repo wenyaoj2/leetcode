6094a0c984902c50d6b55d8e3b4a21c9460c9f72
jinwenyao <wenyaojinapply@gmail.com>
2020-06-03 18:44:27 -0500

    Create 1431. Kids With the Greatest Number of Candies

---
+++ 1431. Kids With the Greatest Number of Candies.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class KidsWithCandies {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = 0;
+        for(int candy : candies) {
+            max = Math.max(max, candy);
+        }
+        List<Boolean> res = new ArrayList();
+        for(int candy : candies) {
+            if(candy + extraCandies >= max) {
+                res.add(true);
+            }
+            else {
+                res.add(false);
+            }
+        }
+        return res;
+    }
+}
