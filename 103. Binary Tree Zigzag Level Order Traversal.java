9ef94424c0c56dcf2f6d9163a5df79ef44fe6b0c
jinwenyao <wenyaojinapply@gmail.com>
2020-07-14 22:22:44 -0500

    Create 103. Binary Tree Zigzag Level Order Traversal

--- 103. Binary Tree Zigzag Level Order Traversal.java
+++ 103. Binary Tree Zigzag Level Order Traversal.java
@@ -1,11 +1,12 @@
+import java.util.*;
+
 /**
  * Definition for a binary tree node. public class TreeNode { int val; TreeNode
  * left; TreeNode right; TreeNode() {} TreeNode(int val) { this.val = val; }
  * TreeNode(int val, TreeNode left, TreeNode right) { this.val = val; this.left
  * = left; this.right = right; } }
  */
-import java.util.*;
-class LevelOrder {
+class ZigzagLevelOrder {
     public class TreeNode {
         int val;
         TreeNode left;
@@ -18,16 +19,17 @@ class LevelOrder {
             this.right = right;
         }
     }
-    public List<List<Integer>> levelOrder(TreeNode root) {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList();
         if(root == null) {
             return res;
         }
+        int level = 0;
         Queue<TreeNode> queue = new LinkedList();
         queue.offer(root);
         while(!queue.isEmpty()) {
-            int len = queue.size();
             List<Integer> list = new ArrayList();
+            int len = queue.size();
             for(int i = 0; i < len; i++) {
                 TreeNode cur = queue.poll();
                 list.add(cur.val);
@@ -38,7 +40,11 @@ class LevelOrder {
                     queue.offer(cur.right);
                 }
             }
-            res.add(new ArrayList(list));
+            if(level % 2 == 1) {
+                Collections.reverse(list);
+            }
+            res.add(list);
+            level++;
         }
         return res;
     }
