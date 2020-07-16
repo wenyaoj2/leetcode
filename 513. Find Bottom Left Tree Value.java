49d53c05d50023a6f2e309cdea074c1a42a4d1cb
jinwenyao <wenyaojinapply@gmail.com>
2020-07-16 16:18:22 -0500

    Create  513. Find Bottom Left Tree Value

---
+++ 513. Find Bottom Left Tree Value.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode() {} TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) { this.val = val; this.left
+ * = left; this.right = right; } }
+ */
+class Solution {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public int findBottomLeftValue(TreeNode root) {
+        int res = 0;
+        Queue<TreeNode> queue = new LinkedList();
+        queue.offer(root);
+        while(!queue.isEmpty()) {
+            int len = queue.size();
+            for(int i = 0; i < len; i++) {
+                TreeNode cur = queue.poll();
+                if(i == 0) {
+                    res = cur.val;
+                }
+                if(cur.left != null) {
+                    queue.offer(cur.left);
+                }
+                if(cur.right != null) {
+                    queue.offer(cur.right);
+                }
+            }
+        }
+        return res;
+    }
+}
