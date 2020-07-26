4c1629be7e93c73b5b91a7ae1b06b9f5b77e5f0d
jinwenyao <wenyaojinapply@gmail.com>
2020-07-26 12:52:22 -0500

    Create 258. Add Digits

---
+++ 258. Add Digits .java
@@ -0,0 +1,13 @@
+class AddDigits {
+    public int addDigits(int num) {
+        if(num < 10) {
+            return num;
+        }
+        int res = 0;
+        while(num > 0) {
+            res += num % 10;
+            num = num / 10;
+        }
+        return addDigits(res);
+    }
+}
